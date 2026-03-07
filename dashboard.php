2b3232716973ec830f92c7d197095d32390130ab
melovink <daydreaminghamburbur@gmail.com>
2026-03-07 23:34:17 +0800

    Added header and ideas for next beautiful day

--- dashboard.php
+++ dashboard.php
@@ -14,6 +14,10 @@
             exit();
         }
     ?>
+
+    <header>
+        <h1>MMR</h1> <!-- Idea : Add a menu to navigate to user's profile so they can RUD it -->
+    </header>
     <form action="logout.php" method="POST">
         <button type="submit">
             Logout
